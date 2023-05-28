e346684c1a8191a0f73fb6039da07447ce8e3431
Matthew Kan <mkan@localhost.localdomain>
2023-05-28 20:26:54 +0100

    Add function to return feedback for guess and filtering eligible words

--- src/wordle/information.clj
+++ src/wordle/information.clj
@@ -1,7 +1,5 @@
-(ns wordle.information)
-
-(require
- '[clojure.math :as math])
+(ns information
+  (:require [clojure.math :as math]))
 
 (defn entropy [probs]
   (reduce + (map
@@ -19,12 +17,38 @@
 (defn sigmoid-weighted-prob [probs]
   (update-all probs sigmoid))
 
+(defn compare-target-to-guess [target guess]
+  (map #(let [current-letter (nth guess %)]
+          (cond
+            (= (nth target %) seeking [:g current-letter])
+            (some (fn [x] (= current-letter x)) target) [:y current-letter]
+            :else [:b current-letter]))
+        (range (count target))))
 
-; search for eligible words
-(defn eligible-words [words feedback]
-  (filter eligible? words))
+[{:g l1} :y l2 :b l3 :b l4 :b l5]
+(defn eligible-by-feedback? [word feedback]
+  (map (fn [w p]
+          (let [present (some (fn [x] (= (nth p 1) x)) word)]
+          (case (first p)
+            :g (= (nth p 1) w)
+            :y (not (= (nth p 1) w))
+            :b (not present))))
+        word
+        feedback))
 
 (defn eligible? [word feedback]
-  )
-; if green match
-; 
+  (every? true? (eligible-by-feedback? word feedback)))
+
+; search for eligible words
+(defn eligible-words [word feedback words]
+  (filter (partial eligible? word feedback) words))
+
+
+(def possible-words
+  (-> (slurp "data/possible_words.txt")
+      (clojure.string/split-lines)
+      ((fn [w]
+         (zipmap w (repeat (count w) (/ 1 (count w))))))))
+
+
+(entropy possible-words)
