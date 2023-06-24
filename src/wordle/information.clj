3ab59d5bec23556664fb4d4a98d5e68dd91a2b8e
Matthew Kan <mkan@localhost.localdomain>
2023-06-24 11:02:30 +0100

    Add comments, move eligible-words function to core

--- src/wordle/information.clj
+++ src/wordle/information.clj
@@ -1,6 +1,11 @@
 (ns wordle.information
   (:require [clojure.math :as math]))
 
+(defn update-all
+  "Updates all values in hash-map"
+  [m f & args]
+  (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m))
+
 (defn entropy [probs]
   "Calculates total entropy/information"
   (reduce + (map
@@ -11,18 +16,11 @@
   "Sigmoid function"
   (/ 1 (+ 1 (math/exp (- x)))))
 
-(defn update-all
-  "Updates all values in hash-map"
-  [m f & args]
-  (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m))
-
 (defn sigmoid-weighted-prob [probs]
   "Apply sigmoid function to probs"
   (update-all probs sigmoid))
 
-
-; search for eligible words
-(defn eligible-words [feedback words]
-  "Filter out ineligible words according to the feedback"
-  (filter (partial eligible? feedback) words))
-
+;; max information:
+;; max probablility under entropy graph across all possible guesses
+;; sigmoid is for incorporating letter frequency
+;; least number of all greys?
